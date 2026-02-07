7aa998c6e4ed6f395b8a8a7c14f9f0b192c2ec6d
Troy Clark <troyclark@OKDVTL42JJHN-Pro.local>
2026-02-07 21:42:38 +0200

    Websockets minimum viable implementation

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,50 @@
-import './styles/main.scss';
+import "./styles/main.scss";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000", {
+	transports: ["websocket"],
+});
+
+socket.on("createdSession", (msg) => {
+	console.log("createdSession:", msg);
+	const sessionId = document.createElement("h1");
+	sessionId.innerText = msg.session_id;
+	document.body.appendChild(sessionId);
+});
 
 const createMatchButton = document.getElementById("create-match");
 const joinMatchButton = document.getElementById("join-match");
 
-createMatchButton.addEventListener("click", createMatchMenu)
+createMatchButton.addEventListener("click", createSessionMenu);
+joinMatchButton.addEventListener("click", joinSessionMenu);
 
-function createMatchMenu(){
-    joinMatchButton.remove();
-    createMatchButton.remove();
-    spawnGameBoard();
+function createSessionMenu() {
+	// joinMatchButton.remove();
+	// createMatchButton.remove();
+	// spawnGameBoard();
+	socket.emit("createSession");
+}
+
+function joinSessionMenu() {
+	joinMatchButton.remove();
+	createMatchButton.remove();
+	const joinDiv = document.createElement("div");
+	joinDiv.classList.add("flex-container", "join-text-input");
+
+	joinDiv.textContent = "Enter Join Code";
+
+	const input = document.createElement("input");
+	joinDiv.appendChild(input);
+
+	const confirmButton = document.createElement("div");
+	confirmButton.classList.add("select-button");
+	confirmButton.textContent = "Join";
+
+	joinDiv.appendChild(confirmButton);
+
+	document.body.appendChild(joinDiv);
+
+	confirmButton.addEventListener("click", () => {
+		socket.emit("joinSession", input.value);
+	});
 }
---
+++ server/server.js
@@ -0,0 +1,81 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+	cors: {
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST"],
+	},
+});
+
+const sessions = new Map();
+let allowedSessionJoinRetries = 6;
+
+function createSessionId() {
+	return crypto.randomUUID().slice(0, 8);
+}
+
+function createSession({ session_id, player_a_id, player_b_id }) {
+	return {
+		session_id,
+		player_a_id,
+		player_b_id,
+		created_at: Date.now(),
+	};
+}
+
+const sleep = (ms) => new Promise((num) => setTimeout(num, ms));
+
+async function waitForPlayerBJoin(sessionId) {
+	for (let retry = 0; retry < allowedSessionJoinRetries; retry++) {
+		const session = sessions.get(sessionId);
+		if (!session) throw new Error("Session not found");
+
+		if (session.player_b_id != null) {
+			console.log("player b joined!");
+			break;
+		}
+
+		console.log("waiting for player b for session ", sessionId);
+		await sleep(5000);
+	}
+
+	console.log("Session timed out");
+	sessions.remove(sessionId);
+	return null;
+}
+
+io.on("connection", (socket) => {
+	console.log("connected:", socket.id);
+
+	socket.on("createSession", (data) => {
+		const sessionId = createSessionId();
+		const session = createSession({
+			session_id: sessionId,
+			player_a_id: socket.id,
+			player_b_id: null,
+		});
+		sessions.set(sessionId, session);
+		socket.emit("createdSession", sessions.get(sessionId));
+		waitForPlayerBJoin(sessionId);
+	});
+
+	socket.on("joinSession", (data) => {
+		const session = sessions.get(data);
+		session.player_b_id = socket.id;
+	});
+
+	socket.emit("beginGame");
+
+	socket.on("disconnect", () => {
+		console.log("disconnected:", socket.id);
+	});
+});
+
+httpServer.listen(3000, () => {
+	console.log("ws server running on localhost:3000");
+});
