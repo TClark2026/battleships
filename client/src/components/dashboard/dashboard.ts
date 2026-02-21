f4373ed1ff0b06b0cb839979bc53dd6f35bed972
Troy Clark <troyclark@OKDVTLFDYLF3.local>
2026-02-21 21:00:45 +0200

    profile tab and logout

--- client/src/components/dashboard/dashboard.ts
+++ client/src/components/dashboard/dashboard.ts
@@ -5,10 +5,10 @@ import "../playerDiv/PlayerDiv";
 import "../inviteList/inviteList";
 import "../inviteDiv/inviteDiv";
 import {
-	playerList$,
-	playerInvites$,
-	type Invite,
-	type GameHistoryItem,
+  playerList$,
+  playerInvites$,
+  type Invite,
+  type GameHistoryItem,
 } from "../../network/sockets";
 import { Subscription } from "rxjs";
 import type { PlayerDiv } from "../playerDiv/PlayerDiv";
@@ -16,15 +16,15 @@ import type { inviteDiv } from "../inviteDiv/inviteDiv";
 import { authStore } from "../../utils/auth";
 
 export class Dashboard extends HTMLElement {
-	private root: ShadowRoot;
-	private lobby: Subscription;
-	private invites: Subscription;
-	private localInvites: Invite[] = [];
-	constructor() {
-		super();
-		this.root = this.attachShadow({ mode: "open" });
+  private root: ShadowRoot;
+  private lobby: Subscription;
+  private invites: Subscription;
+  private localInvites: Invite[] = [];
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: "open" });
 
-		this.root.innerHTML = `
+    this.root.innerHTML = `
     <style>${styles}</style>
 	<app-navbar></app-navbar>
 	<div class="flex-container">
@@ -32,101 +32,142 @@ export class Dashboard extends HTMLElement {
 	<invite-list></invite-list>
 	</div>
     `;
-		this.lobby = playerList$.subscribe((players) => {
-			const playerContainer = this.root.querySelector("player-list");
-			if (!playerContainer) return;
+    this.lobby = playerList$.subscribe((players) => {
+      const playerContainer = this.root.querySelector("player-list");
+      if (!playerContainer) return;
 
-			playerContainer.innerHTML = "";
-			for (const player of players) {
-				const playerElem = document.createElement("player-div") as PlayerDiv;
-				playerElem.username = player.username;
-				playerContainer.appendChild(playerElem);
-			}
-		});
+      playerContainer.innerHTML = "";
+      for (const player of players) {
+        const playerElem = document.createElement("player-div") as PlayerDiv;
+        playerElem.username = player.username;
+        playerContainer.appendChild(playerElem);
+      }
+    });
 
-		this.invites = playerInvites$.subscribe((invites) => {
-			if (JSON.stringify(invites) !== JSON.stringify(this.localInvites)) {
-				this.localInvites = invites;
+    this.invites = playerInvites$.subscribe((invites) => {
+      if (JSON.stringify(invites) !== JSON.stringify(this.localInvites)) {
+        this.localInvites = invites;
 
-				const inviteContainer = this.root.querySelector("invite-list");
-				if (!inviteContainer) return;
+        const inviteContainer = this.root.querySelector("invite-list");
+        if (!inviteContainer) return;
 
-				inviteContainer.innerHTML = "";
+        inviteContainer.innerHTML = "";
 
-				for (const invite of this.localInvites) {
-					const inviteElem = document.createElement("invite-div") as inviteDiv;
-					inviteElem.username = invite.from;
-					inviteElem.inviteId = invite.inviteId;
-					inviteContainer.appendChild(inviteElem);
-				}
-			}
-		});
-	}
+        for (const invite of this.localInvites) {
+          const inviteElem = document.createElement("invite-div") as inviteDiv;
+          inviteElem.username = invite.from;
+          inviteElem.inviteId = invite.inviteId;
+          inviteContainer.appendChild(inviteElem);
+        }
+      }
+    });
+  }
 
-	connectedCallback() {
-		this.root
-			.querySelector("app-navbar")!
-			.addEventListener("navigate-history", () => {
-				this.getGameHistory();
-			});
+  connectedCallback() {
+    this.root
+      .querySelector("app-navbar")!
+      .addEventListener("navigate-history", () => {
+        this.getGameHistory();
+      });
 
-		this.root
-			.querySelector("app-navbar")!
-			.addEventListener("navigate-lobby", () => {
-				const lobbyDash = this.root.querySelector(".flex-container");
-				lobbyDash?.replaceChildren();
-				const players = document.createElement("player-list");
-				const invites = document.createElement("invite-list");
-				lobbyDash?.appendChild(players);
-				lobbyDash?.appendChild(invites);
-			});
-	}
+    this.root
+      .querySelector("app-navbar")!
+      .addEventListener("navigate-profile", () => {
+        this.getProfile();
+      });
 
-	getGameHistory() {
-		const storageKey = "gameHistory";
+    this.root
+      .querySelector("app-navbar")!
+      .addEventListener("navigate-lobby", () => {
+        const lobbyDash = this.root.querySelector(".flex-container");
+        lobbyDash?.replaceChildren();
+        const players = document.createElement("player-list");
+        const invites = document.createElement("invite-list");
+        lobbyDash?.appendChild(players);
+        lobbyDash?.appendChild(invites);
+      });
+  }
 
-		const data: Record<string, GameHistoryItem[]> = JSON.parse(
-			localStorage.getItem(storageKey) ?? "{}",
-		);
+  getProfile() {
+    const profileContainer = document.createElement("div");
+    profileContainer.classList.add("flex-container");
 
-		const matchHistoryData = Object.values(data).flat();
+    const username = authStore.getUser()!.username;
+    const profile = document.createElement("h2");
+    profile.innerText = "You are: " + username;
 
-		const matchHistory = document.createElement("div");
-		matchHistory.classList.add("flex-container");
-		for (const match of matchHistoryData) {
-			const matchDiv = document.createElement("div");
-			matchDiv.classList.add("match");
-			const winner = document.createElement("h3");
-			winner.classList.add("winner");
-			if (match.winner === authStore.getUser()?.username) {
-				matchDiv.classList.add("win");
-				winner.innerText = "You";
-			} else {
-				matchDiv.classList.add("loss");
-				winner.innerText = match.winner;
-			}
-			const reason = document.createElement("p");
-			reason.innerText = "Reason: " + match.reason;
-			matchDiv.appendChild(winner);
-			matchDiv.appendChild(reason);
-			matchHistory.appendChild(matchDiv);
-		}
-		this.root.querySelector(".flex-container")?.replaceChildren(matchHistory);
-	}
+    const logoutButton = document.createElement("button");
+    logoutButton.innerText = "Log Out";
+    logoutButton.classList.add("logout-button");
+    logoutButton.addEventListener("click", () => {
+      location.reload();
+      authStore.clearAuth();
+    });
+    profileContainer.appendChild(profile);
+    profileContainer.appendChild(logoutButton);
 
-	disconnectedCallback() {
-		this.lobby?.unsubscribe();
+    this.root
+      .querySelector(".flex-container")
+      ?.replaceChildren(profileContainer);
+  }
 
-		this.invites?.unsubscribe();
+  getGameHistory() {
+    const storageKey = "gameHistory";
 
-		const playerContainer = this.root.querySelector("player-list");
-		if (playerContainer) playerContainer.innerHTML = "";
+    const data: Record<string, GameHistoryItem[]> = JSON.parse(
+      localStorage.getItem(storageKey) ?? "{}",
+    );
 
-		const inviteContainer = this.root.querySelector("invite-list");
-		if (inviteContainer) inviteContainer.innerHTML = "";
+    const matchHistoryData = Object.values(data).flat();
 
-		this.localInvites = [];
-	}
+    const matchHistory = document.createElement("div");
+    matchHistory.classList.add("flex-container");
+
+    if (matchHistoryData.length === 0) {
+      const emptyHeading = document.createElement("h2");
+      emptyHeading.innerText = "No History Available";
+      matchHistory.appendChild(emptyHeading);
+    } else {
+      for (const match of matchHistoryData) {
+        const matchDiv = document.createElement("div");
+        matchDiv.classList.add("match");
+
+        const winner = document.createElement("h3");
+        winner.classList.add("winner");
+
+        if (match.winner === authStore.getUser()?.username) {
+          matchDiv.classList.add("win");
+          winner.innerText = "You";
+        } else {
+          matchDiv.classList.add("loss");
+          winner.innerText = match.winner;
+        }
+
+        const reason = document.createElement("p");
+        reason.innerText = "Reason: " + match.reason;
+
+        matchDiv.appendChild(winner);
+        matchDiv.appendChild(reason);
+        matchHistory.appendChild(matchDiv);
+      }
+    }
+
+    this.root.querySelector(".flex-container")?.replaceChildren(matchHistory);
+  }
+
+  disconnectedCallback() {
+    this.lobby?.unsubscribe();
+
+    this.invites?.unsubscribe();
+
+    const playerContainer = this.root.querySelector("player-list");
+    if (playerContainer) playerContainer.innerHTML = "";
+
+    const inviteContainer = this.root.querySelector("invite-list");
+    if (inviteContainer) inviteContainer.innerHTML = "";
+
+    this.localInvites = [];
+  }
 }
 
 customElements.define("app-dashboard", Dashboard);
--- client/src/components/navbar/navbar.ts
+++ client/src/components/navbar/navbar.ts
@@ -1,40 +1,50 @@
 import styles from "./navbar.scss?inline";
 export class Navbar extends HTMLElement {
-	private root: ShadowRoot;
+  private root: ShadowRoot;
 
-	constructor() {
-		super();
-		this.root = this.attachShadow({ mode: "open" });
+  constructor() {
+    super();
+    this.root = this.attachShadow({ mode: "open" });
 
-		this.root.innerHTML = `
+    this.root.innerHTML = `
     <style>${styles}</style>
     <div>
         <span class="lobby">Lobby</span>
         <span class="match-history">Match History</span>
+		<span class="profile">Profile</span>
     </div>
     
     `;
-	}
+  }
 
-	connectedCallback() {
-		this.root.querySelector(".match-history")?.addEventListener("click", () => {
-			this.dispatchEvent(
-				new CustomEvent("navigate-history", {
-					bubbles: true,
-					composed: true,
-				}),
-			);
-		});
+  connectedCallback() {
+    this.root.querySelector(".profile")?.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("navigate-profile", {
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
 
-		this.root.querySelector(".lobby")?.addEventListener("click", () => {
-			this.dispatchEvent(
-				new CustomEvent("navigate-lobby", {
-					bubbles: true,
-					composed: true,
-				}),
-			);
-		});
-	}
+    this.root.querySelector(".match-history")?.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("navigate-history", {
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
+
+    this.root.querySelector(".lobby")?.addEventListener("click", () => {
+      this.dispatchEvent(
+        new CustomEvent("navigate-lobby", {
+          bubbles: true,
+          composed: true,
+        }),
+      );
+    });
+  }
 }
 
 customElements.define("app-navbar", Navbar);
